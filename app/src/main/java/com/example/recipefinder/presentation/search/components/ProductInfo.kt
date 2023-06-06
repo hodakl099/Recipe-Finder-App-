2bcfd2c0062a1cec75acb6c190d416d5ab7a73b8
mahmo <mahmoud.alkateb22@gmail.com>
2023-06-06 18:06:07 +0200

    project dependency setup

--- app/src/main/java/com/example/recipefinder/presentation/search/components/ProductInfo.kt
+++ app/src/main/java/com/example/recipefinder/presentation/search/components/ProductInfo.kt
@@ -1,12 +1,13 @@
 package com.example.recipefinder.presentation.search.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.RowScopeInstance.weight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import coil.compose.AsyncImage
+import com.example.recipefinder.R
 
 @Composable
 fun ProductInfoItem(
@@ -17,7 +18,16 @@ fun ProductInfoItem(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
+            AsyncImage(
+                modifier = Modifier.weight(1f),
+                model = "",
+                contentDescription = "Image from api",
+                placeholder = painterResource(id = R.drawable.ic_launcher_foreground)
+            )
+            Column() {
 
+
+            }
         }
     }
 }
